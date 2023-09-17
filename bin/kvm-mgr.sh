28a080b708accd04c59cbf7ec0cbdd91f260e73b
Timothy C. Arland <tcarland@gmail.com>
2023-09-17 11:05:39 -0600

    sync

--- bin/kvm-mgr.sh
+++ bin/kvm-mgr.sh
@@ -3,7 +3,7 @@
 #  Creates and manages KVM infrastructure using a JSON manifest.
 #
 PNAME=${0##*\/}
-VERSION="v23.04"
+VERSION="v23.09"
 AUTHOR="Timothy C. Arland <tcarland@gmail.com>"
 
 pool="default"
