cc679482c19c4f8f7d1ef53dff777752bc380df0
Timothy C. Arland <tcarland@gmail.com>
2023-11-29 14:55:50 -0700

    add edit passthrough cmd

--- bin/kvm-mgr.sh
+++ bin/kvm-mgr.sh
@@ -3,7 +3,7 @@
 #  Creates and manages KVM infrastructure using a JSON manifest.
 #
 PNAME=${0##*\/}
-VERSION="v23.09"
+VERSION="v23.11"
 AUTHOR="Timothy C. Arland <tcarland@gmail.com>"
 
 pool="default"
