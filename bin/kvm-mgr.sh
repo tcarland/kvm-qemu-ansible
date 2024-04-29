d7bd8189d17497f1044c4e04d9861c02d7a8e8b8
Timothy C. Arland <tcarland@gmail.com>
2024-04-29 08:30:10 -0600

    add action workflow

--- bin/kvm-mgr.sh
+++ bin/kvm-mgr.sh
@@ -3,7 +3,7 @@
 #  Creates and manages KVM infrastructure using a JSON manifest.
 #
 PNAME=${0##*\/}
-VERSION="v24.04"
+VERSION="v24.05"
 AUTHOR="Timothy C. Arland <tcarland@gmail.com>"
 
 pool="default"
