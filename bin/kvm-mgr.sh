abd3835626e7c9e60664cda0fdcdc421fc247c9d
Timothy C. Arland <tcarland@gmail.com>
2025-01-21 09:11:33 -0700

    set noble as default

--- bin/kvm-mgr.sh
+++ bin/kvm-mgr.sh
@@ -3,7 +3,7 @@
 #  Creates and manages KVM infrastructure using a JSON manifest.
 #
 PNAME=${0##*\/}
-VERSION="v24.08"
+VERSION="v25.01"
 AUTHOR="Timothy C. Arland <tcarland@gmail.com>"
 
 pool="default"
