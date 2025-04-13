1f18a4e94f2ad004958e2dbb6d932e29d29e8dc8
Timothy C. Arland <tcarland@users.noreply.github.com>
2025-04-13 12:13:41 -0600

    drop use of ansible subdir (#20)

--- bin/kvm-mgr.sh
+++ bin/kvm-mgr.sh
@@ -3,7 +3,7 @@
 #  Creates and manages KVM infrastructure using a JSON manifest.
 #
 PNAME=${0##*\/}
-VERSION="v25.03.16"
+VERSION="v25.04.13"
 AUTHOR="Timothy C. Arland <tcarland@gmail.com>"
 
 pool="default"
